7bedf9470172eadde4522632c1d9b63ce1d02f27
Nicholas Buckeridge <bucknich@gmail.com>
2023-11-05 12:37:04 +1000

    Updated the packages for this template project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,15 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
 <script>
-    import '../app.css';
+	import '../app.css';
+
+	import Button from '../stories/Button.svelte';
 </script>
 
+<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Button label="clickme">Click me</Button>
+
 <style lang="postcss">
-    :global(html) {
-        background-color: theme(colors.gray.200);
-    }
+	:global(html) {
+		background-color: theme(colors.gray.200);
+	}
 </style>
